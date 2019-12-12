7c5a0d2fc62967b7435f0fba78a195ecf50ad7c5
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-12 03:43:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
   # your code here
   new_array = []
-  new_array.push
-  array.each = do |number|
-   number ** 2
+  array.each  do 
+    new_array << array ** 2 
  end 
 end
 
